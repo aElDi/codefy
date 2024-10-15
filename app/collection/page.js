6e2c0814d33932df75aad95a204a175e88aed8bf
aElDi <varfst1995s@gmail.com>
2024-10-16 00:03:31 +0400

    [HOTFIX] Hotfix #5 Fixed `dynamic` parameter on `/collection` page. Now it dynamic and will be re-render every request!

--- app/collection/page.js
+++ app/collection/page.js
@@ -1,6 +1,7 @@
 import CollectionBrowser from "@/components/CollectionBrowser";
 import db from "@/lib/db";
 
+export const dynamic = 'force-dynamic'
 export default async function CollectionPage() {
   const links = await db.collection("links").find({}).toArray();
 
