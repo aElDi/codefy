0a1da1c90064069bc2ab17394a88c7d615d0dde1
0tdaysalo <pasha20051344@gmail.com>
2024-10-16 21:16:36 +0400

    [NEW] Append security like/unlike 🔏

--- app/api/like/[id]/route.js
+++ app/api/like/[id]/route.js
@@ -1,11 +1,27 @@
 import db from "@/lib/db";
 import { NextResponse } from "next/server";
+
 export async function GET(request, { params }) {
+
+    //получение ip
+    const user_ip = request.ip + request.headers.get('User-Agent'); 
     const id = Number(params.id);
-    try{
+
+
+    const like_exist = await db.collection("likes").findOne({ ip: user_ip, link_id: id });
+    if (like_exist) {
+        //если лайк есть то нельзя ставить
+        return new NextResponse("ALREADY EXIST", { status: 400 });
+    }
+
+    try {
         await db.collection("links").findOneAndUpdate({id: id}, {
             $inc: { likes: 1 }
         });
+
+        //сохраняем инфу о лайке
+        await db.collection("likes").insertOne({ ip: user_ip, link_id: id });
+
         return new NextResponse("OK")
     } catch (e) {
         console.error(e)
--- app/api/unlike/[id]/route.js
+++ app/api/unlike/[id]/route.js
@@ -1,8 +1,22 @@
 import db from "@/lib/db";
 import { NextResponse } from "next/server";
+
 export async function GET(request, { params }) {
+
+    //получение ip
+    const user_ip =  request.ip + request.headers.get('User-Agent');
     const id = Number(params.id);
-    try{
+
+    //проверка есть ли лайк от чела
+    const like_exist = await db.collection("likes").findOne({ ip: user_ip, link_id: id });   
+    if (!like_exist) {
+        return NextResponse("DID NOT LIKED", { status: 400 });
+    } 
+    try {
+
+        //удаление одного
+        await db.collection("likes").deleteOne({ ip: user_ip, link_id: id });
+
         await db.collection("links").findOneAndUpdate({id: id}, {
             $inc: { likes: -1 }
         });
